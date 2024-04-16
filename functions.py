818e5b2e9efdc843ccdce53dfb1aa955b168b11c
Totoro <lolmerci4@gmail.com>
2024-04-16 19:03:07 +0200

    correction entry

--- functions.py
+++ functions.py
@@ -2,23 +2,29 @@ import tkinter
 from tkinter import *
 
 def entryValid():
+    var = False
     print("entryValid lancée")
-    if len(nbVilles.get())>0 and len(nbVilles.get())<=2 and (nbVilles.get())!="0" and (nbVilles.get())<="50" and (nbVilles.get()).isdigit():
-        print("à true")
-        bouton.config(state = tkinter.ACTIVE)
-        #return True
-    else:
-        print("à false")
+    if len(nbVilles.get())>0 and len(nbVilles.get())<=2 and (nbVilles.get())!="0" and (nbVilles.get()).isdigit():
+        if int(nbVilles.get())<=50:
+            print(f"Vous avez choisi {nbVilles.get()} villes")
+            var = True
+    if var == False:
+        print("Le nombre de villes doit être compris entre 1 et 50")
         bouton.config(state = tkinter.DISABLED)
-        #return False
+    else:
+        bouton.config(state = tkinter.ACTIVE)
     
 def graphe():
+    villes = int(nbVilles.get())
     print("fonction graphe lancée")
     bouton.config(state = tkinter.DISABLED) # -> bouton grisé = bouton non cliquable
+    print(f"{villes}")
+    result = 3 + villes
+    print(f"{result}")
         
 fenetre = tkinter.Tk()
 fenetre.title("Affichage du graphe")
-fenetre.minsize(width=350, height=275)
+fenetre.minsize(width=216, height=47)
 fenetre.configure(pady=10)
     
 txt = tkinter.Label(text = "Nombre de villes :")
